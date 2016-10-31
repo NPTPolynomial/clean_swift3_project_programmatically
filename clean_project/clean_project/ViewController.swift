351db6c63ac9fc67b7d96fa4ec0ef4904770fad4
NPTPolynomial <chengcao8@gmail.com>
2016-10-31 13:21:23 -0700

    clean project 1

---
+++ clean_project/clean_project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clean_project
+//
+//  Created by Aidan on 2016-10-31.
+//  Copyright © 2016 Aidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
